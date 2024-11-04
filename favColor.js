af6a49a07d8f42bb1461cd8fad99508fb007adc5
lemfritz11 <aintdaiki@gmail.com>
2024-11-04 21:16:48 +0800

    initial commit with favColor.js

---
+++ favColor.js
@@ -0,0 +1,7 @@
+const favoriteColors = [];
+
+while (favoriteColors.length < 3) {
+    let color = prompt("Enter a color you like:");
+    favoriteColors.push(color);
+    console.log(favoriteColors);
+}
